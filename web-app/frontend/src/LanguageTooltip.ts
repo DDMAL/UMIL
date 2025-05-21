e9e117a13d3c4b1aea9ad1917e6712c315167bd1
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-05-21 11:45:45 -0400

    build: set up `django-vite`
    - Removed bootstrap from dependency and used CDN instead. Otherwise, custom CSS will not overwrite bootstrap styles
    - Cleaned up unused code in templates and ts files
    - `django-vite` supports HMR for TS files, but not for CSS and images yet

--- web-app/frontend/src/LanguageTooltip.ts
+++ web-app/frontend/src/LanguageTooltip.ts
@@ -1,8 +1,16 @@
-import { Tooltip } from 'bootstrap';
+// Make this file a module to allow global augmentation
+export {};
+
+// Add a type declaration for window.bootstrap
+declare global {
+  interface Window {
+    bootstrap: any;
+  }
+}
 
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
 const tooltipList = [...tooltipTriggerList].map(
-  (tooltipTriggerEl) => new Tooltip(tooltipTriggerEl)
+  (tooltipTriggerEl) => new (window as any).bootstrap.Tooltip(tooltipTriggerEl)
 );
