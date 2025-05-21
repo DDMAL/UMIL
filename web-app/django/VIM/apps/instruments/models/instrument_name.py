4296868decf456d0b6785ff859c15905046deca7
mailynmailyn <mlpuittinen@gmail.com>
2025-05-21 11:58:39 -0400

    feat: update instrument name with boolean flag indicating new data (is_verified)
    refs: #206 and #205

--- web-app/django/VIM/apps/instruments/models/instrument_name.py
+++ web-app/django/VIM/apps/instruments/models/instrument_name.py
@@ -8,3 +8,4 @@ class InstrumentName(models.Model):
     source_name = models.CharField(
         max_length=50, blank=False, help_text="Who or what called the instrument this?"
     )  # Stand-in for source data; format TBD
+    is_verified = models.BooleanField(default=False)
