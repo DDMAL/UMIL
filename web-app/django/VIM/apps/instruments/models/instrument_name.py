ed0c907d46bcf7297611a686c5f68b410f5bbebd
mailynmailyn <mlpuittinen@gmail.com>
2025-05-29 16:02:13 -0400

    feat: InstrumentName fields - add status field (verified, unverified, uploaded): differentiates new data, verified data and data that already exists on wikidata - contributor field: indicates user that added the InstrumentName
    refs: #206

--- web-app/django/VIM/apps/instruments/models/instrument_name.py
+++ web-app/django/VIM/apps/instruments/models/instrument_name.py
@@ -8,5 +8,24 @@ class InstrumentName(models.Model):
     source_name = models.CharField(
         max_length=50, blank=False, help_text="Who or what called the instrument this?"
     )  # Stand-in for source data; format TBD
-    is_verified = models.BooleanField(default=False)
-    is_alias = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("verified", "Verified"),
+            ("unverified", "Unverified"),
+            ("uploaded", "Uploaded"),
+        ],
+        default="unverified",
+        help_text="Status of the name entry",
+    )
+    is_alias = models.BooleanField(
+        default=False,
+        help_text="Is this an alias for the instrument? If true, it will not be used as the main name.",
+    )
+    contributor = models.ForeignKey(
+        "auth.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        help_text="User who contributed this name",
+    )
