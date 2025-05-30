c22835fba5e15931522a57499d52ca3c35cc2fcc
mailynmailyn <mlpuittinen@gmail.com>
2025-05-30 13:02:03 -0400

    feat: use django admin to restrict user editing access - a django user of type "staff" and group "viewer" can only edit status info of a InstrumentName - display InstrumentName names in django admin view
    refs: #206 and #215

--- web-app/django/VIM/apps/instruments/admin.py
+++ web-app/django/VIM/apps/instruments/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from VIM.apps.instruments.models import Instrument, InstrumentName, Language, AVResource
 
 admin.site.register(Instrument)
-admin.site.register(InstrumentName)
+# admin.site.register(InstrumentName)
 admin.site.register(Language)
 admin.site.register(AVResource)
+
+@admin.register(InstrumentName)
+class InstrumentNameAdmin(admin.ModelAdmin):
+    list_filter = ("status",) # Filter by status
+    search_fields = ("name", "source_name", "instrument__wikidata_id") # Search by name, source name, and instrument wikidata ID
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Make all fields except 'status' read-only for users in the 'viewer' group.
+        """
+        if request.user.groups.filter(name="viewer").exists():
+            return ("instrument", "language", "name", "source_name", "is_alias", "contributor")
+        return super().get_readonly_fields(request, obj)
+    
