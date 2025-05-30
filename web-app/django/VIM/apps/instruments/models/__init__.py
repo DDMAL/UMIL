115c38d34464c74d5b1c7bcf9813d953fdb78e04
mailynmailyn <mlpuittinen@gmail.com>
2025-05-30 12:56:42 -0400

    fix: remove InstrumentAlias
    refs: #206

--- web-app/django/VIM/apps/instruments/models/__init__.py
+++ web-app/django/VIM/apps/instruments/models/__init__.py
@@ -2,4 +2,3 @@ from VIM.apps.instruments.models.instrument import Instrument
 from VIM.apps.instruments.models.instrument_name import InstrumentName
 from VIM.apps.instruments.models.language import Language
 from VIM.apps.instruments.models.avresource import AVResource
-from VIM.apps.instruments.models.instrument_alias import InstrumentAlias
