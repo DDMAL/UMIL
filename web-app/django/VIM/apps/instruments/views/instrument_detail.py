d89add3796b6c011c18059fa396d95468e6191e5
mailynmailyn <mlpuittinen@gmail.com>
2025-06-25 10:52:11 -0400

    feat: add new instrument name - add confirmation modal - read in user input using javascript instead of a model form - remove model form
    refs: #245

--- web-app/django/VIM/apps/instruments/views/instrument_detail.py
+++ web-app/django/VIM/apps/instruments/views/instrument_detail.py
@@ -1,9 +1,5 @@
 from django.views.generic import DetailView
-from VIM.apps.instruments.models import Instrument, Language, InstrumentName
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from ..forms.add_name_form import NameForm
-from django.shortcuts import get_object_or_404
+from VIM.apps.instruments.models import Instrument, Language
 
 class InstrumentDetail(DetailView):
     """
@@ -13,7 +9,6 @@ class InstrumentDetail(DetailView):
     model = Instrument
     template_name = "instruments/detail.html"
     context_object_name = "instrument"
-    form_class = NameForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -36,44 +31,4 @@ class InstrumentDetail(DetailView):
 
         context["languages"] = Language.objects.all()
 
-        context["form"] = NameForm(
-            initial={
-                "instrument": context["instrument"].wikidata_id
-            }
-        )
         return context
-    
-    def post(self, request, **kwargs):
-        instrument = get_object_or_404(Instrument, pk=kwargs.get("pk"))
-        form = NameForm(request.POST)
-        if form.is_valid():
-            language_code = form.cleaned_data["language"]
-            entry = {
-                "name": form.cleaned_data["name"],
-                "source": form.cleaned_data["source_name"],
-            }
-            # Save instrument name into database
-            # If the instrument already has a name in specified language, save as alias
-            if instrument.instrumentname_set.filter(language=language_code).exists():
-                InstrumentName.objects.create(
-                instrument=instrument,
-                language=language_code,
-                name=entry["name"],
-                source_name=entry["source"],
-                is_alias= True,
-                contributor=request.user,
-            )
-            # If the instrument does not have a name in specified language, save as primary name    
-            else:
-                InstrumentName.objects.create(
-                instrument=instrument,
-                language=language_code,
-                name=entry["name"],
-                source_name=entry["source"],
-                is_alias= False,
-                contributor=request.user,
-            )
-            # Redirect to the instrument detail page
-            return HttpResponseRedirect("/instrument/" + str(instrument.pk) + "/")
-
-        return render(request, self.template_name, {"form": form})
