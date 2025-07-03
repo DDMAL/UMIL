95627026f033cf692fc77d5b8c6ce702a1e563fe
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-07-03 15:28:25 -0400

    chore: reformat frontend files

--- web-app/frontend/src/instruments/DeleteName.ts
+++ web-app/frontend/src/instruments/DeleteName.ts
@@ -22,10 +22,8 @@ deleteNameModal.addEventListener('show.bs.modal', function (event) {
       'data-instrument-language',
     );
     var instrumentName = button.getAttribute('data-instrument-name');
-    var instrumentSource = button.getAttribute(
-      'data-instrument-source');
-    instrumentNameId = button.getAttribute(
-      'data-instrument-id');
+    var instrumentSource = button.getAttribute('data-instrument-source');
+    instrumentNameId = button.getAttribute('data-instrument-id');
 
     deleteNameModal.querySelector('#instrumentNameInModal').textContent =
       instrumentName;
@@ -34,42 +32,45 @@ deleteNameModal.addEventListener('show.bs.modal', function (event) {
     deleteNameModal.querySelector('#instrumentLanguageInModal').textContent =
       instrumentNameLanguage;
   }
-
 });
 
-document.getElementById('confirmDeleteBtn').addEventListener('click', function () {
-  const csrftoken = (document.querySelector('[name=csrfmiddlewaretoken]') as HTMLInputElement).value;
+document
+  .getElementById('confirmDeleteBtn')
+  .addEventListener('click', function () {
+    const csrftoken = (
+      document.querySelector('[name=csrfmiddlewaretoken]') as HTMLInputElement
+    ).value;
 
-  // Send the request to publish
-  fetch(`/delete-name/`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRFToken': csrftoken,
-    },
-    body: JSON.stringify({
-      instrument_name_id: instrumentNameId,
-      // publish_to_wikidata: publishToWikidata,
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.status === 'success') {
-        // Close both modals
-        const deleteNameModal = Modal.getInstance(
-          document.getElementById('deleteNameModal'),
-        );
-
-        if (deleteNameModal) {
-          deleteNameModal.hide(); // Close the 'Add Name' modal
-        }
-      } else {
-        alert('Error: ' + data.message);
-      }
+    // Send the request to publish
+    fetch(`/delete-name/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify({
+        instrument_name_id: instrumentNameId,
+        // publish_to_wikidata: publishToWikidata,
+      }),
     })
-    .catch((error) => {
-      alert('An error occurred while deleting the data: ' + error.message);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'success') {
+          // Close both modals
+          const deleteNameModal = Modal.getInstance(
+            document.getElementById('deleteNameModal'),
+          );
+
+          if (deleteNameModal) {
+            deleteNameModal.hide(); // Close the 'Add Name' modal
+          }
+        } else {
+          alert('Error: ' + data.message);
+        }
+      })
+      .catch((error) => {
+        alert('An error occurred while deleting the data: ' + error.message);
+      });
 
     window.location.reload(); // Reload the page to reflect changes
-});
+  });
