d89454410357ddfba1428fc0182a5042c61feff5
mailynmailyn <mlpuittinen@gmail.com>
2025-07-15 13:15:19 -0400

    refactor: batch upload instrument names to UMILdb - limit sql queries - well defined exceptions - prevent database additions until all information is valid and available

--- web-app/django/VIM/apps/instruments/views/update_umil_db.py
+++ web-app/django/VIM/apps/instruments/views/update_umil_db.py
@@ -35,7 +35,7 @@ def add_name(request):
     Returns:
         JsonResponse: JSON response with status and message
     """
-    # Parse the JSON request body
+    # Parse the JSON request body, if it fails return missing data error
     data = json.loads(request.body)
     wikidata_id = data.get("wikidata_id")
     entries = data.get("entries", [])
@@ -44,54 +44,77 @@ def add_name(request):
             {
                 "status": "error",
                 "message": "Missing required data",
-            }
+            },
+            status=400,
         )
-    try:
-        # Fetch the instrument from the database
+    # Fetch the instrument from the database, if it does not exist return does not exist error
+    try:   
         instrument = Instrument.objects.get(wikidata_id=wikidata_id)
-
-        # Process each entry: save to UMIL database
-        for entry in entries:
-            # Extract data from the entry
-            language_code = entry["language"]
-            language = Language.objects.get(wikidata_code=language_code)
-
-            name = entry["name"]
-            source = entry["source"]
-
-            # If the instrument already has a name in specified language, save as alias
-            if instrument.instrumentname_set.filter(language=language).exists():
-                InstrumentName.objects.create(
-                    instrument=instrument,
-                    language=language,
-                    name=name,
-                    source_name=source,
-                    is_alias=True,
-                    contributor=request.user,
-                )
-            # If the instrument does not have a name in specified language, save as primary name
-            else:
-                InstrumentName.objects.create(
-                    instrument=instrument,
-                    language=language,
-                    name=name,
-                    source_name=source,
-                    is_alias=False,
-                    contributor=request.user,
-                )
-
+    except Instrument.DoesNotExist:
         return JsonResponse(
             {
-                "status": "success",
-                "message": "Data saved successfully!",
-            }
+                "status": "error", 
+                "message": "Instrument not found"
+            },
+            status=404,    
+        )
+    
+    # create dictionary to map language codes to Language objects
+    language = {lang.wikidata_code: lang for lang in Language.objects.all()}
+
+    instrument_names_to_create = []
+
+    for entry in entries:
+        language_code = entry["language"]
+        name = entry["name"]
+        source = entry["source"]
+        
+        # Validate that entry info is provided
+        if not name or not source or not language_code:
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": "Missing entry information",
+                },
+                status=400,
+            )
+        
+        # Find language object from language code
+        if language.get(language_code) is None:
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": f"Language '{language_code}' not found",
+                },
+                status=400,
+            )
+
+
+        # Check if the instrument already has a name in the specified language
+        is_alias = instrument.instrumentname_set.filter(language__wikidata_code=language_code).exists()
+
+        # Prepare the InstrumentName object
+        instrument_names_to_create.append(
+            InstrumentName(
+                instrument=instrument,
+                language=language.get(language_code),
+                name=name,
+                source_name=source,
+                is_alias=is_alias,
+                contributor=request.user,
+            )
         )
 
-    except Instrument.DoesNotExist:
-        return JsonResponse({"status": "error", "message": "Instrument not found"})
-    except Exception as e:
-        return JsonResponse({"status": "error", "message": "" + str(e)})
+    # Bulk create all InstrumentName objects
+    InstrumentName.objects.bulk_create(instrument_names_to_create)
 
+    return JsonResponse(
+        {
+            "status": "success",
+            "message": "All entries saved successfully",
+        },
+        status=200,
+    )
 
 @login_required
 @require_http_methods(["DELETE"])
@@ -117,7 +140,22 @@ def delete_name(request):
 
         # If user is a superuser or created the name, allow deletion
         if request.user.is_superuser or instrument_name.contributor == request.user:
-            instrument_name.delete() 
+            instrument_name.delete()
+            return JsonResponse(
+                {
+                    "status": "success",
+                    "message": "Instrument name deleted successfully",
+                },
+                status = 200,
+            )
+        elif instrument_name.contributor is None:
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": "This name was not created by any user, it cannot be deleted",
+                },
+                status = 403,
+            )
         else: 
             return JsonResponse(
                 {
@@ -126,12 +164,12 @@ def delete_name(request):
                 },
                 status = 403,
             )
-        return JsonResponse({"status": "success"})
-    
+        
     except InstrumentName.DoesNotExist:
         return JsonResponse(
-            {"status": "error", "message": "Instrument name not found"},
+            {
+                "status": "error", 
+                "message": "Instrument name not found",
+            },
             status = 404,
         )
-    except Exception as e:
-        return JsonResponse({"status": "error", "message": "" + str(e)})
