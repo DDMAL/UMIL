474913ec940554ec3ffba56c9cf08321a567865d
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-07-24 15:15:34 -0400

    feat: add jump-to-top button to instruments & detail page
    refs: #300

--- web-app/frontend/vite.config.js
+++ web-app/frontend/vite.config.js
@@ -29,6 +29,7 @@ export default defineConfig({
           __dirname,
           'src/instruments/DisplaySettings.ts',
         ),
+        jumpToTop: resolve(__dirname, 'src/instruments/JumpToTop.ts'),
         languageList: resolve(__dirname, 'src/LanguageList.ts'),
         statsAnimation: resolve(__dirname, 'src/stats/StatsAnimation.ts'),
         barCharts: resolve(__dirname, 'src/stats/BarCharts.ts'),
