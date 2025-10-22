a05de14e67642f9da3a119e3c5f636dce2ec5d3f
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-10-22 17:02:56 -0400

    test: add navigation E2E test for desktop and mobile

--- web-app/frontend/playwright.config.ts
+++ web-app/frontend/playwright.config.ts
@@ -29,26 +29,31 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
       dependencies: ['setup'],
+      testMatch: /.*\/desktop\.spec\.ts/,
     },
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
       dependencies: ['setup'],
+      testMatch: /.*\/desktop\.spec\.ts/,
     },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
       dependencies: ['setup'],
+      testMatch: /.*\/desktop\.spec\.ts/,
     },
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
       dependencies: ['setup'],
+      testMatch: /.*\/mobile\.spec\.ts/,
     },
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
       dependencies: ['setup'],
+      testMatch: /.*\/mobile\.spec\.ts/,
     },
   ],
 });
