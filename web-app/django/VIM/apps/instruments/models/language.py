9e6eced2c27e40139bd82f5a1631612a4294759a
PouyaMohseni <pouyamohseniii@gmail.com>
2025-12-01 17:34:51 -0500

    feat: add RTL language support to instrument detail page
    Sparql Wikidata to retrieve language direction in import_languages.py.
    Add direction field to the Language model and inject dir=rtl/ltr
    for each row in the instrument detail.
    
    resolves #427

--- web-app/django/VIM/apps/instruments/models/language.py
+++ web-app/django/VIM/apps/instruments/models/language.py
@@ -9,6 +9,15 @@ class Language(models.Model):
     autonym = models.CharField(
         blank=False, help_text="Language label in the language itself"
     )
+    direction = models.CharField(
+        max_length=3,
+        choices=[
+            ("ltr", "Left-to-Right"),
+            ("rtl", "Right-to-Left"),
+        ],
+        default="ltr",
+        help_text="Text direction",
+    )
 
     def __str__(self):
         return f"{self.en_label}"
