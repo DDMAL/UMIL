e49d7334e4b817e344d647bee7ba9b1e72e26fc9
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-12-17 16:58:08 -0500

    test: increase timeout in CI

--- web-app/frontend/playwright.config.ts
+++ web-app/frontend/playwright.config.ts
@@ -3,7 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/e2e',
   timeout: 30 * 1000,
-  expect: { timeout: 5000 },
+  expect: {
+    timeout: process.env.CI ? 10000 : 5000,
+  },
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
